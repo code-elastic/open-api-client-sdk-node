3c9902edf0de7685881152f51397a713f6eada07
HZreal <hzreal0823@gmail.com>
2024-06-20 23:48:47 +0800

    refactor: index export, test

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * @author huang
+ * @date 2024-05-20
+ * @File: index.js.py
+ * @Description:
+ */
+const OpenClient = require('./lib/openClient');
+module.exports = OpenClient;
---
+++ invoke.test.js
@@ -0,0 +1,4 @@
+const OpenClient = require('./index');
+
+new OpenClient('qwert123456', 'asdfgqwertzxcvb').getNameByPost('zzzzz')
+
